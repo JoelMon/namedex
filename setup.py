ead7c1708d6cce4ec31e39980468bb20eb24bc52
Joel Montes de Oca <6587811+JoelMon@users.noreply.github.com>
2019-06-14 16:56:29 -0400

    Bump version: 0.1.21 → 0.1.22

--- setup.py
+++ setup.py
@@ -42,6 +42,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/JoelMon/namedex',
-    version='0.1.21',
+    version='0.1.22',
     zip_safe=False,
 )
